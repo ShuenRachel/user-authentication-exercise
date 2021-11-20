285056dcaa13cc3f238c36423683a53e9043d58d
ShuenRachel <shuen0410@gmail.com>
2021-11-20 17:10:33 +0800

    feat: add users.json

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const { engine } = require('express-handlebars')
+const userList = require('./users.json')
+
+app.use(express.urlencoded({ extended: true }))
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -14,6 +17,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  // const { email, password } = req.body
   res.render('login')
 })
 
